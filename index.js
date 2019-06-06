f6e923523732682bc336ee9685b57fbff42d0400
AzySir <azy.sir@gmail.com>
2019-06-06 14:32:41 +1000

    getPets() function done

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ function run(url) {
     axios.get(url)
     .then(response => {
         maleArray = getGender(response.data, response.data.length, "Male");
-        console.log("----Male----")
-        console.log(maleArray);
-        console.log("----END OF MALE----")
         femaleArray = getGender(response.data, response.data.length, "Female");
-        console.log("----Female----")
-        console.log(femaleArray);
-        console.log("----END OF FEMALE----")
 
-        sortedMaleArray = sortAlphabetical(maleArray);
-        sortedFemaleArray = sortAlphabetical(femaleArray)
+
+        console.log("----- Male Cat -----")
+        sortedMaleArray = getCat(maleArray);
+        console.log("----- END OF Male Cat -----")
+        console.log("----- Female Cat -----")
+        sortedFemaleArray = getCat(femaleArray);
+        console.log("----- END Of Female Cat -----")
+
+        // sortedMaleArray = sortAlphabetical(maleArray);
+        // sortedFemaleArray = sortAlphabetical(femaleArray);
         
     })
 }
@@ -32,6 +34,34 @@ function run(url) {
     }
     return genderArr;
   }
+
+  function getCat(custJSON) {
+    catArray = []
+    petsArray = getPets(custJSON);
+    petTypeArray = []; 
+    for (var i = 0; i < petsArray.length; i++) {
+        petTypeArray.push(petsArray[i].pets);
+    }
+
+    
+  }
+
+  function getPets(custJSON) {
+    petsArray = [];
+    var count = 0;
+    for (var i = 0; i < custJSON.length; i++) {
+        if (custJSON[i].pets != null) {
+            petsArray.push(custJSON[i]);
+        }
+        else {
+            count++;
+        }
+    }
+
+    console.log(petsArray)
+    return petsArray;
+  }
+
 
   
   

e90c5047f7d1b2ff08106608d9483cca55cab180
AzySir <azy.sir@gmail.com>
2019-06-06 15:25:16 +1000

    Added some QA In

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-
-// var http = require("http");
-var axios = require('axios');
-var url = "http://agl-developer-test.azurewebsites.net/people.json";
-var maleJSON = "";
-var femaleJSON = "";
+var axios = require('axios'); //Include Axios Library for JS handling
+const url = "http://agl-developer-test.azurewebsites.net/people.json"; //Set API 
+const selectedPet = "Cat" //Change this configuration variable to get the Pet of your selection
 
 function run(url) {
-    axios.get(url)
-    .then(response => {
-        maleArray = getGender(response.data, response.data.length, "Male");
-        femaleArray = getGender(response.data, response.data.length, "Female");
+    axios.get(url) //Get API 
+    .then(response => { //Then do -
+        
+        maleArray = getGender(response.data, "Male"); //Set Male Genders into an Array
+        femaleArray = getGender(response.data, "Female"); //Set Female Genders into an Array
  
-        catMaleArray = getCat(maleArray);
-        catFemaleArray = getCat(femaleArray);
+        catMaleArray = getAnimal(maleArray, selectedPet); //Set an array from the list of Males with cats
+        catFemaleArray = getAnimal(femaleArray, selectedPet); //Set an array from the list of Females with cats 
 
-        sortedMaleArray = sortAlphabetical(catMaleArray);
-        sortedFemaleArray = sortAlphabetical(catFemaleArray);
+        sortedMaleArray = sortAlphabetical(catMaleArray); //Sort the Male Array alphabetically
+        sortedFemaleArray = sortAlphabetical(catFemaleArray); //Sort the Female Array alphabetically
 
-        displayCats("Male", sortedMaleArray)
-        console.log("      ")
-        displayCats("Female", sortedFemaleArray)
+        if (sortedMaleArray.length > 0) { //Will only display Male if the array is greater than 0
+            displayCats("Male", sortedMaleArray) //Print out Cats with Male Owners
+            console.log("      ") //Seperator on the output
+        }
+
+        if (sortedFemaleArray.length > 0) { //Will only display Female if the array is greater than 0
+            displayCats("Female", sortedFemaleArray) //Print out Cats with Female Owners
+            console.log("      ") //Seperator on the output
+        }
     })
 }
 
-  function getGender(custJSON, jsonLength, gender) {
-      let genderArr = [];
-    for (var i = 0; i < jsonLength; i++) {
-        if (custJSON[i].gender == gender) {
-            genderArr.push(custJSON[i])
+    function getGender(custJSON, gender) {
+      let genderArr = []; //init Array
+    for (var i = 0; i < custJSON.length; i++) { //Loop through the JSON 
+        if (custJSON[i].gender == gender) { //If gender of "customer" is Male/Female
+            genderArr.push(custJSON[i]) //Add to gender Array
         }
     }
-    return genderArr;
+    return genderArr; //return genderArray to be handled
   }
 
-  function getCat(custJSON) {
-    catArray = []
+  function getAnimal(custJSON, animal) {
+    animalArray = [] //Init Arrays
     petTypeArray = []; 
-    petArray = getPet(custJSON);
-    for (var i = 0; i < petArray.length; i++) {
-           for (var j = 0; j < petArray[i].pets.length; j++) {
-                if (petArray[i].pets[j].type == "Cat") {
-                    catArray.push(petArray[i].pets[j].name);
+    petArray = getPet(custJSON); //get petArray
+    for (var i = 0; i < petArray.length; i++) { //Loop through Customer Array
+           for (var j = 0; j < petArray[i].pets.length; j++) { //Loop through the Pets details
+                if (petArray[i].pets[j].type.toLocaleLowerCase() == animal.toLocaleLowerCase()) { //If pet Type == cat
+                    animalArray.push(petArray[i].pets[j].name);
                 }    
            }
     }
     
-    return catArray;
+    return animalArray;
   }
 
   function getPet(custJSON) {
@@ -67,10 +71,15 @@ function run(url) {
   }
 
   function displayCats(gender, catArr) {
-    console.log(gender)
-    for (var i = 0; i < catArr.length; i++) {
-        console.log(" * " + catArr[i]);
+    if (catArr == null) {
+        console.log(Gender + " has nothing to show as there was no Cat's detected")
+    }
+    else {
+        console.log(gender)
+        for (var i = 0; i < catArr.length; i++) {
+            console.log(" * " + catArr[i]);
+        }
     }
   }
 
-run(url)
+run(url) //Run the main code
